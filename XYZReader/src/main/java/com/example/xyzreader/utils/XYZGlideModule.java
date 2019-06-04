8871466fcf79a76488a7c4e5c3208e79f664a38b
Osaigbovo <odiase.osaigbovo@gmail.com>
2019-06-04 08:22:17 +0100

    added Glide

---
+++ XYZReader/src/main/java/com/example/xyzreader/utils/XYZGlideModule.java
@@ -0,0 +1,8 @@
+package com.example.xyzreader.utils;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public final class XYZGlideModule extends AppGlideModule {
+}
